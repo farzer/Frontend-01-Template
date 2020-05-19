33352aa55f96542928e7ef5d3b6251b46f28a1f4
farzer <huaentan@gmail.com>
2020-05-19 13:22:02 +0800

    fix: update week 03

--- week03/作业.js
+++ week03/作业.js
@@ -4,7 +4,7 @@
  * @param str 字符串
  * @param x 进制
  */
-function convertStringToNumber(str: string, x: number) {
+function convertStringToNumber(str, x) {
   if (arguments.length < 2) {
     x = 10
   }
@@ -12,24 +12,26 @@ function convertStringToNumber(str: string, x: number) {
   let chars = str.split('')
   let number = 0
   let i = 0
+  // 整数部分
   while (i < chars.length && chars[i] !== '.') {
     number *= x
     number += chars[i].codePointAt(0) - '0'.codePointAt(0)
     i++
   }
 
+  // 小数部分
   if (chars[i] === '.') {
     i++
   }
-
   let fraction = 1
   while (i < chars.length) {
     fraction /= x;
-    number += (chars[i].codePointAt(0) - '0'.codePointAt(0))
+    number += (chars[i].codePointAt(0) - '0'.codePointAt(0)) + fraction
     i++
   }
 
-  return number
+
+  return number + fraction;
 }
 
 // 2. 数字转字符串
@@ -38,15 +40,26 @@ function convertStringToNumber(str: string, x: number) {
  * @param num 数字
  * @param x 进制
  */
- function convertNumberToString(num: number, x: number) {
+function convertNumberToString(num, x) {
    let integer = Math.floor(num)
   let fraction = num - integer
-  let str = ''
+  let str = integer ? '' + '0'
+  // 整数部分
   while (integer > 0) {
-    str += integer % x
+    str = String(integer % x) + str
     integer = Math.floor(integer / x)
   }
 
   // handle fraction
+  if (fraction > 0) {
+    str += '.'
+  }
+
+  while (fraction > 0) {
+    fraction *= x;
+    str += Math.floor(fraction)
+    fraction -= Math.floor(fraction)
+  }
+
   return str
 }
