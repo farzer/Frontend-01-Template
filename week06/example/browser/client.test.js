deadd18d8108f35e950d30ee3dac48da0fb55b2f
farzer <huaentan@gmail.com>
2020-05-20 23:59:59 +0800

    refactor: week06 & add jest configuration

---
+++ week06/example/browser/client.test.js
@@ -0,0 +1,46 @@
+const Request = require('./client');
+const parser = require('./parser')
+async function makeRequest() {
+  const req = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    path: '/',
+    port: 8188,
+    headers: {
+      'X-Type': 'big'
+    },
+    body: {
+      field: 1,
+      test: 2
+    }
+  })
+
+  let dom = null
+  
+  try {
+    const res = await req.send()
+    dom = parser.parseHTML(res.body)
+  } catch (error) {
+    console.log(error)
+  }
+
+  return dom
+}
+
+describe('HTTP browser', () => {
+  let result = null
+  beforeAll((done) => {
+    makeRequest().then(res => {
+      result = res
+      done()
+    })
+  })
+
+  test('should be null', () => {
+    expect(null).toBeNull()
+  });
+
+  test('should be null', () => {
+    expect(result).toBeNull()
+  });
+})
