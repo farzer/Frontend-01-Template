8f0ed8f3d6c0b44b7404d062c284bef0be7678b1
farzer <huaentan@gmail.com>
2020-05-20 23:24:52 +0800

    feat: week06 toy browser

---
+++ week06/example/browser/client.js
@@ -0,0 +1,24 @@
+const Request = require('../../../week05/example/client');
+const parser = require('./parser')
+void async function() {
+  const req = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    path: '/',
+    port: 8188,
+    headers: {
+      'X-Type': 'big'
+    },
+    body: {
+      field: 1,
+      test: 2
+    }
+  })
+  
+  try {
+    const res = await req.send()
+    let dom = parser.parseHTML(res.body)
+  } catch (error) {
+    console.log(error)
+  }
+}()
