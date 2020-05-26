36b5fc5ba85abba6a3587bd16e76d4bba51321e2
farzer <huaentan@gmail.com>
2020-05-26 15:44:38 +0800

    feat: 完成 DOM 树的构建

---
+++ week06/example/step/6-constructTree/parser.spec.js
@@ -0,0 +1,34 @@
+const parser = require('./parser')
+
+const html = `<html maaa=a ><head><style>
+body div #myid{width:100px;}</style></head>
+<body><div class="sss" disabled='true' ><img id="myid"/><img /></div></body>
+</html>`
+
+describe('Step 3: parse Tag', () => {
+  let state = null, tokenArr = []
+  beforeAll(() => {
+    const { state: resState, tokenArr: resTokenArr } = parser.parseHTML(html);
+    state = resState
+    tokenArr = resTokenArr
+  })
+  test('state should EOF Symbol', () => {
+    expect(typeof state).toBe('symbol')
+  });
+
+  test('tokenArr should contain two img tagName', () => {
+    const imgs = tokenArr.filter(token => token.tagName === 'img')
+    expect(imgs.length).toBe(2)
+  });
+
+  test('the first img token should contain an id attribute', () => {
+    const imgs = tokenArr.filter(token => token.tagName === 'img')
+    expect(imgs[0].id).toBeDefined()
+  });
+
+  test('debug stack', () => {
+    const imgs = tokenArr.filter(token => token.tagName === 'img')
+    // 去掉下面注释以观察 stack 的变化
+    // expect(imgs[0].id).not.toBeDefined()
+  });
+});
